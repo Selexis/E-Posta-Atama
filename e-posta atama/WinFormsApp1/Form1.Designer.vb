065df8fa1d0e7b035a11989c83075a8dae07ec65
Yavuz Selim Sarıhan <yavuzselim.s584@gmail.com>
2024-04-20 00:45:06 +0300

    E-Posta ve Numara Atama
    Yeni kayıt oluşturulmuş üniversite öğrencilerine e-posta ve numara atama.

---
+++ e-posta atama/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        adtxt = New TextBox()
+        soyadtxt = New TextBox()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        unitxt = New ComboBox()
+        fakultetxt = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(58, 29)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(28, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Ad"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(36, 66)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(50, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Soyad"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 110)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(74, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Üniversite"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(31, 160)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(55, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Fakulte"
+        ' 
+        ' adtxt
+        ' 
+        adtxt.Location = New Point(92, 22)
+        adtxt.Name = "adtxt"
+        adtxt.Size = New Size(125, 27)
+        adtxt.TabIndex = 4
+        ' 
+        ' soyadtxt
+        ' 
+        soyadtxt.Location = New Point(92, 59)
+        soyadtxt.Name = "soyadtxt"
+        soyadtxt.Size = New Size(125, 27)
+        soyadtxt.TabIndex = 5
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Location = New Point(288, 45)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(398, 244)
+        ListBox1.TabIndex = 8
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(36, 241)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(187, 27)
+        Button1.TabIndex = 9
+        Button1.Text = "E Posta Ve Numara Ata"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' unitxt
+        ' 
+        unitxt.FormattingEnabled = True
+        unitxt.Items.AddRange(New Object() {"Anadolu Üniversitesi", "Ondokuz Mayıs Üniversitesi", "Gazi Üniversitesi"})
+        unitxt.Location = New Point(92, 110)
+        unitxt.Name = "unitxt"
+        unitxt.Size = New Size(151, 28)
+        unitxt.TabIndex = 10
+        ' 
+        ' fakultetxt
+        ' 
+        fakultetxt.FormattingEnabled = True
+        fakultetxt.Items.AddRange(New Object() {"Tıp Fakultesi", "Egitim" & ChrW(127) & " Fakultesi", "Hukuk Fakultesi", "Muhendislik Fakultesi"})
+        fakultetxt.Location = New Point(92, 157)
+        fakultetxt.Name = "fakultetxt"
+        fakultetxt.Size = New Size(151, 28)
+        fakultetxt.TabIndex = 11
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(fakultetxt)
+        Controls.Add(unitxt)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Controls.Add(soyadtxt)
+        Controls.Add(adtxt)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents adtxt As TextBox
+    Friend WithEvents soyadtxt As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents unitxt As ComboBox
+    Friend WithEvents fakultetxt As ComboBox
+End Class
---
+++ e-posta atama/WinFormsApp1/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Dim tipsayac, egitimsayac, hukuksayac, muhsayac, sayac As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ad, soyad, uni, eposta As String
+        Dim n As Integer
+
+        ad = Strings.Left(adtxt.Text, 3)
+        soyad = Strings.Right(soyadtxt.Text, 3)
+        n = Strings.InStr(unitxt.Text, " ")         ' kaçıncı karakter olduğu boşluktan itibaren karakterli al
+        uni = Strings.Left(unitxt.Text, (n - 1))         ' soldan itibaren karakterlerini al n-1
+
+        eposta = Strings.LCase(ad & soyad & "@" & uni & ".edu.tr")
+        eposta = Strings.Replace(eposta, "ş", "s")
+        eposta = Strings.Replace(eposta, "ı", "i")
+        eposta = Strings.Replace(eposta, "ü", "u")
+        eposta = Strings.Replace(eposta, "ö", "o")
+        eposta = Strings.Replace(eposta, "ç", "c")
+        eposta = Strings.Replace(eposta, "ğ", "g")
+        eposta = Strings.Replace(eposta, "ondokuz", "omu") 'ondokuz yazısı varsa onu omu olarak değiştir
+        eposta = Strings.Replace(eposta, "anadolu", "and")
+
+
+
+        Dim fakultekodu, yil, sira As String 'numara atama 
+
+        yil = Strings.Right(System.DateTime.Today.Year, 2) '2023  sistem saati 2023 yılındaysa 23 bilgisini al 
+
+        If fakultetxt.Text = "Tıp Fakultesi" Then
+            fakultekodu = "01"
+            tipsayac += 1
+            sayac = tipsayac
+
+        ElseIf fakultetxt.Text = "Egitim Fakultesi" Then
+            fakultekodu = "02"
+            egitimsayac += 1
+            sayac = egitimsayac
+
+        ElseIf fakultetxt.Text = "Hukuk Fakultesi" Then
+            fakultekodu = "03"
+            hukuksayac += 1
+            sayac = hukuksayac
+
+        ElseIf fakultetxt.Text = "Muhendislik Fakultesi" Then
+            fakultekodu = "04"
+            muhsayac += 1
+            sayac = muhsayac
+        End If
+
+        'formuller buradaydı'
+
+        sira = Format(sayac, "000") 'format yazmaktansa sayacı 3 haneli yaz
+
+        ListBox1.Items.Add(adtxt.Text & " " & soyadtxt.Text)
+        ListBox1.Items.Add(eposta & "  " & yil & fakultekodu & sira)
+
+
+
+    End Sub
+
+End Class
+
+
+'If Strings.Len(sayac.ToString) = 1 Then
+'    sira = "000" & sayac.ToString
+
+'ElseIf Strings.Len(sayac.ToString) = 2 Then
+'    sira = "00" & sayac.ToString
+
+'ElseIf Strings.Len(sayac.ToString) = 3 Then
+'    sira = "0" & sayac.ToString
+
+'ElseIf Strings.Len(sayac.ToString) = 4 Then
+'    sira = sayac.ToString
+'End If
+
+
